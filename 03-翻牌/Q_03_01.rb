147f502cd7c8e102574471c6710c83b8361112f7
silence <154261688@qq.com>
2019-05-31 01:38:38 +0800

    03-翻转

---
+++ 03-翻牌/Q_03_01.rb
@@ -0,0 +1,37 @@
+#!/usr/bin/env ruby
+# -*- coding:utf-8 -*-
+
+# 算法实现终稿
+def reversal ()
+    n = 100
+    (1..n).each {|i|
+        flag = false
+        (2..n).each{|j|
+            if i % j == 0       
+                flag = !flag
+            end
+        }
+        puts "#{i}" if flag==false
+    }
+end
+
+# 算法实现 01
+def reversal_01 ()
+    n = 100
+    cards = Array.new(n, false)
+
+    # 从2到n翻牌
+    (2..n).each {|i|
+        j = i - 1
+        while j < cards.size do
+            cards[j] = !cards[j]
+            j += i
+        end
+    }
+    n.times{|i|
+        puts i + 1 if !cards[i]
+    }
+end
+
+reversal()
+reversal_01()
