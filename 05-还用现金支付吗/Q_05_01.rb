f7b6fa9257e738e099f27d5799694729fbcd07d2
silence <154261688@qq.com>
2019-06-10 08:55:09 +0800

    question_05

---
+++ 05-还用现金支付吗/Q_05_01.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+# -*- coding: utf-8 -*-
+
+coins = [10, 50, 100, 500]
+cnt = 0
+
+puts "====#{coins.shift}"
+
+(2..15).each do |i|
+    coins.repeated_combination(i).each do |coin_set|
+        cnt += 1 if coin_set.inject(:+) == 1000
+    end
+end
+
+puts cnt
