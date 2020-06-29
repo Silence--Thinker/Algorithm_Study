8a462feada92976dad941c0f9a55914f89ece9e0
silence <154261688@qq.com>
2020-06-29 19:44:57 +0800

    项目结构变更

---
+++ LeetCode/01-两数之和/Solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> ans;
+        map<int, int>hashmap;
+        for (int i = 0; i < nums.size(); i++) {
+            if (hashmap[target - nums[i]] && hashmap[target - nums[i]] != i + 1) {
+                ans.push_back(i);
+                ans.push_back(hashmap[target - nums[i]] - 1);
+                return ans;
+            }
+        hashmap[nums[i]] = i + 1
+        }
+        return ans;
+    }
+};
