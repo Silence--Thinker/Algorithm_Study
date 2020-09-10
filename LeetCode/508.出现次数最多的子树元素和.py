0b4cbee43d2706d3989c749180ddb7391891f0c3
silence <154261688@qq.com>
2020-09-10 15:23:59 +0800

    出现次数最多的子树元素和

---
+++ LeetCode/508.出现次数最多的子树元素和.py
@@ -0,0 +1,23 @@
+#
+# @lc app=leetcode.cn id=508 lang=python
+#
+# [508] 出现次数最多的子树元素和
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def findFrequentTreeSum(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        
+# @lc code=end
+
